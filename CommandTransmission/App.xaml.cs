8e6c5c9f288a29fcbcbd004e436b1d3241e2a382
yd_private <690462878@qq.com>
2019-11-17 17:20:29 +0800

    temp

--- CommandTransmission/App.xaml.cs
+++ CommandTransmission/App.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using CommandTransmission.IO;
+using System;
 using System.Collections.Generic;
 using System.Configuration;
 using System.Data;
@@ -19,6 +20,8 @@ namespace CommandTransmission
 
             var bootstrapper = new Bootstrapper();
             bootstrapper.Run();
+
+            WriteToPort.SendMsg();
         }
     }
 }
---
+++ CommandTransmission/IO/WriteToPort.cs
@@ -0,0 +1,35 @@
+﻿using RabbitMQ.Client;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CommandTransmission.IO
+{
+    public static class WriteToPort
+    {
+        public static void SendMsg()
+        {
+            //int i = 0;
+            //while (true)
+            //{
+            //    var factory = new ConnectionFactory() { HostName = "39.108.177.237", Port = 5672, UserName = "admin", Password = "admin", VirtualHost = "/" };
+            //    using (var connection = factory.CreateConnection())
+            //    using (var channel = connection.CreateModel())
+            //    {
+            //        channel.ExchangeDeclare(exchange: "",
+            //                                type: "topic");
+            //        var body = Encoding.UTF8.GetBytes(i.ToString());
+            //        channel.BasicPublish(exchange: "topic_logs",
+            //                             routingKey: "anonymous.info",
+            //                             basicProperties: null,
+            //                             body: body);
+            //        Console.WriteLine("asfd");
+            //    }
+            //}
+
+        }
+
+    }
+}
